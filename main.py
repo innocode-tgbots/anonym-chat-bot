b1a874deb537be09f819a3bff00229543a1911cf
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2023-07-08 10:19:48 +0300

    fix: fix bug with early matching

--- main.py
+++ main.py
@@ -28,11 +28,14 @@ async def start_handler(message: types.Message):
             user_id=user_id
         )  # Создание объекта User и добавление в словарь
 
-    await message.answer(
+    await message.reply(
         text="Привет! Я бот для знакомств. Напиши мне своё имя, чтобы найти себе пару!"
     )
 
     await OurStates.enter_name.set()  # Установка состояния enter_name
+
+
+# dp.register_message_handler(start_handler, commands="start")
 
 
 @dp.message_handler(
@@ -40,11 +43,10 @@ async def start_handler(message: types.Message):
 )  # Обработчик для ввода имени пользователя
 async def enter_name_handler(message: types.Message):
     user_id = message.from_id
-    user_mapping[
-        user_id
-    ].name = message.text  # Сохранение введенного имени пользователя
+    user = user_mapping[user_id]
+    user.name = message.text  # Сохранение введенного имени пользователя
 
-    text = f"Отлично, {user_mapping[user_id].name}! Сейчас я попробую найти тебе пару."
+    text = f"Отлично, {user.name}! Сейчас я попробую найти тебе пару."
     await message.answer(text=text)
     await message.answer(text="Ты готов? Напиши 'да', если да, и 'нет', если нет.")
     await OurStates.yes_or_no.set()  # Установка состояния yes_or_no
@@ -55,7 +57,9 @@ async def enter_name_handler(message: types.Message):
     # будет работать только если текст сообщения равен "да" или "yes"
     state=OurStates.yes_or_no  # будет работать только в состоянии yes_or_no
 )  # Обработчик для ответа "да"
-async def wait_for_partner_handler(message: types.Message):
+async def wait_for_partner_handler(message: types.Message, ):
+    await OurStates.wait_for_partner.set()  # Установка состояния wait_for_partner
+
     user = user_mapping[message.from_id]
     chosen = None  # Переменная для хранения идентификатора партнера
 
@@ -63,13 +67,22 @@ async def wait_for_partner_handler(message: types.Message):
     for partner_id, partner_obj in user_mapping.items():
         # Проверка, что пользователь не связан с самим собой
         # и что у партнера нет связи с другим пользователем
-        if partner_id != user.user_id and partner_obj.partner_id is None:
-            chosen = partner_id
+        if partner_id != user.user_id and partner_obj.partner_id is None and partner_obj.name is not None:
+            # Получение текущего контекста партнера
+            partner_context = dp.get_current().current_state(chat=partner_id, user=partner_id)
+            # Где он находится на графе состояний
+            partner_state = await partner_context.get_state()
+            # Если он в состоянии "ожидает партнера", то выбираем его
+            if partner_state == OurStates.wait_for_partner.state:
+                chosen = partner_id
+                break
 
     if chosen is not None:
         # Установка связи между пользователями
+        partner = user_mapping[chosen]
+
         user.partner_id = chosen
-        user_mapping[chosen].partner_id = user.user_id
+        partner.partner_id = user.user_id
 
         await message.answer(
             text=f"Ты сейчас связан с {user_mapping[chosen].name}! Напиши что-нибудь."
